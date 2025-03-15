ca5857a125d3fb18932382f0805f2fde9dffb7a8
sanjeev <sanjeevvasu81@gmail.com>
2025-03-15 11:11:12 +0530

    first commit

---
+++ src/main/java/com/model/AppController.java
@@ -0,0 +1,19 @@
+package com.model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class AppController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, Sanjeev!";
+	}
+	
+	
+
+}
